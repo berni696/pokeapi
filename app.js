2eb55de1a48e0065189117a4fe735f2b74ff53e2
berni696 <bernat696@gmail.com>
2022-11-06 00:17:41 +0100

    WIP :: add pagination

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
 addEventListener('load', (event) => {
+
     const app = document.getElementById('app');
-
-    axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40')
-    .then(function (response) {
-        for (pokemon of response.data.results) {
-
-                axios.get(pokemon.url)
-                .then(function (response) {
-                    app.innerHTML += 
-                    `
-                    <div class="pokemon-box" id="${response.data.id}">
-                        <img src="${response.data.sprites.front_default}" alt="${response.data.name}">
-                        <p>${response.data.name}</p>
-                    </div>
-                    `;
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-                .then(function () {
-                    // always executed
-                });
-        }  
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .then(function () {
-        // always executed
-    });
-
+    let page = document.getElementById("pagination");
+    let pageValue = page.dataset.value*10;
     
-    const sum = require('./pokemonsApi');
-    console.log(sum)
+    const index = () => {
+        
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=' + pageValue + '&limit=10')
+        .then(function (response) {
+            for (pokemon of response.data.results) {
+
+                    axios.get(pokemon.url)
+                    .then(function (response) {
+                        app.innerHTML += 
+                        `
+                        <div class="pokemon-box" id="${response.data.id}">
+                            <img src="${response.data.sprites.front_default}" alt="${response.data.name}">
+                            <p>${response.data.name}</p>
+                        </div>
+                        `;
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    })
+                    .then(function () {
+                        // always executed
+                    });
+            }  
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+    }
+
+    index()
+
+    page.addEventListener("click", index);
+
 
 });
 
